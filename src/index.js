f3b0ff42b461d380f71a86f01ffc3534e39be5a5
Aleksandr Lobkov <das149848@gmail.com>
2022-02-22 14:03:53 +0400

    feat: create function reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let newnumber = String(n);
+    if (n < 0) {
+        newnumber = String(Math.abs(n));
+    }
+    if (n % 10 == 0) {
+        newnumber = newnumber.slice(0, -1);
+    }
+    return Number(newnumber.split("").reverse().join(""));
 }
